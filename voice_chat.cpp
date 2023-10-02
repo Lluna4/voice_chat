c85f8db696e3e3ae76e3725a7ea22b490ab814fc
Luna <69774917+Lluna4@users.noreply.github.com>
2023-10-02 15:19:44 +0200

    Added networking

--- voice_chat.cpp
+++ voice_chat.cpp
@@ -1,19 +1,28 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#undef _WINSOCKAPI_
+#define _WINSOCKAPI_
 #include <iostream>
 #include <Windows.h>
 #include <fstream>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "winmm.lib")
+
+const std::string SERVER_IP = "127.0.0.1";
+const int BUFFER_SIZE = 8192;
+
+SOCKET client_socket;
 
 void CALLBACK WaveInProc(HWAVEIN hwi, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
 {
     if (uMsg == WIM_DATA)
     {
         WAVEHDR* waveHdr = (WAVEHDR*)dwParam1;
-        std::ofstream outputFile("captured_audio.raw", std::ios::binary | std::ios::app); // Open the file in binary append mode
-        if (outputFile.is_open())
-        {
-            // Write the captured audio data to the file
-            outputFile.write(waveHdr->lpData, waveHdr->dwBytesRecorded);
-            outputFile.close();
-        }
+        int bytes_send = send(client_socket, waveHdr->lpData, waveHdr->dwBufferLength, 0);
+        std::cout << waveHdr->dwBufferLength << std::endl;
         waveInAddBuffer(hwi, waveHdr, sizeof(WAVEHDR));
     }
 }
@@ -21,11 +30,30 @@ void CALLBACK WaveInProc(HWAVEIN hwi, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR
 
 int main()
 {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        return 1;
+    }
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(SERVER_IP.c_str());
+    server_address.sin_port = htons(5050);
+
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == SOCKET_ERROR)
+    {
+		std::cerr << "Connect failed: " << WSAGetLastError();
+        closesocket(client_socket);
+        WSACleanup();
+    }
+
     WAVEFORMATEX wfx;
     wfx.wFormatTag = WAVE_FORMAT_PCM;
     wfx.nChannels = 2;
-    wfx.nSamplesPerSec = 44100;
-    wfx.nAvgBytesPerSec = 44100 * 2 * sizeof(short);
+    wfx.nSamplesPerSec = 16000;
+    wfx.nAvgBytesPerSec = 16000 * 2 * sizeof(short);
     wfx.nBlockAlign = 2 * sizeof(short);
     wfx.wBitsPerSample = 16;
     wfx.cbSize = 0; 
@@ -38,8 +66,8 @@ int main()
         std::cout << "Audio input device opened successfully." << std::endl;
         
         WAVEHDR waveHdr;
-        waveHdr.lpData = (LPSTR)malloc(8192 * sizeof(short));
-        waveHdr.dwBufferLength = 8192 * sizeof(short);
+        waveHdr.lpData = (LPSTR)malloc(BUFFER_SIZE * sizeof(short));
+        waveHdr.dwBufferLength = BUFFER_SIZE * sizeof(short);
         waveHdr.dwFlags = 0;
 
         waveInPrepareHeader(hWaveIn, &waveHdr, sizeof(WAVEHDR));
@@ -65,3 +93,13 @@ int main()
         std::cerr << "Failed to open audio input device." << std::endl;
     }
 }
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
---
+++ voice_chat_sv.cpp
@@ -0,0 +1,78 @@
+#undef _WINSOCKAPI_
+#define _WINSOCKAPI_
+#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+#include <vector>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int PORT = 5050;
+const std::string SERVER_IP = "0.0.0.0";
+const int BUFFER_SIZE = 8192 * 2;
+
+std::vector<int> clients;
+
+void manage_server(int socket)
+{
+    char *buffer = (char *)calloc(BUFFER_SIZE, sizeof(char));
+    int bytes_read = 0;
+    while (1)
+    {
+        bytes_read = recv(socket, buffer, BUFFER_SIZE, 0);
+        std::cout << bytes_read << std::endl;
+        if (bytes_read == -1)
+        {
+            break;
+        }
+        if (buffer)
+        {
+            std::ofstream outputFile("captured_audio.raw", std::ios::binary | std::ios::app); // Open the file in binary append mode
+            if (outputFile.is_open())
+            {
+                // Write the captured audio data to the file
+                outputFile.write(buffer, bytes_read);
+                outputFile.close();
+            }
+        }
+        memset(buffer, 0, BUFFER_SIZE);
+    }
+}
+
+int main()
+{
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("WSAStartup failed: %d\n", WSAGetLastError());
+        return 1;
+    }
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == 0)
+    {
+        std::cout << "Se fallo al crear socket" << std::endl;
+        return -1;
+    }
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, SERVER_IP.c_str(), &address.sin_addr);
+    address.sin_port = htons(PORT);
+    if (bind(sock, (struct sockaddr*)&address, sizeof(address)) < 0)
+    {
+        std::cout << "Bind ha fallado" << std::endl;
+    }
+    std::cout << "Escuchando conexiones" << "en "<< PORT << std::endl;
+    while (true)
+    {
+        std::cout << "AAAA" << std::endl;
+        listen(sock, 5);
+        int new_socket = accept(sock, (struct sockaddr*)&address, &addrlen);
+        std::cout << new_socket << std::endl;
+        clients.push_back(new_socket);
+        std::thread man_sv(manage_server, new_socket);
+        man_sv.detach();
+    }
+}
